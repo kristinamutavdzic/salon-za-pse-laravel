2ee22d48e451f9884fda25293cdf1a1f29bec435
Kristina Mutavdzic <mj20190244@student.fon.bg.ac.rs>
2023-03-23 23:42:41 +0100

    Kreirani modeli i njihove veze

---
+++ salon/app/Models/Frizura.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Frizura extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'naziv',
+        'vreme',
+        'tezinaFrizure',
+    ];
+
+    public function termin()
+    {
+        return $this->hasMany(Termin::class);
+    }
+}
